ced822c07339eafd442ad963a682b42a4af13202
Colby Harris <harris.colby@gmail.com>
2022-03-13 19:56:34 -0500

    added most api routes and db saving

---
+++ routes/apiRoutes/index.js
@@ -0,0 +1,20 @@
+const router = require('express').Router();
+const req = require('express/lib/request');
+const notes = require('../../db/db.json');
+const { v4: uuidv4 } = require('uuid');
+const createNewNote = require ('../../lib/notes')
+
+
+router.get('/notes', (req, res) => {
+  res.json(notes);
+});
+
+router.post('/notes', (req, res) => {
+  req.body.id = uuidv4();
+
+  res.json(req.body);
+  const noteList = createNewNote(req.body);
+  // res.json(noteList);
+});
+
+module.exports = router;
